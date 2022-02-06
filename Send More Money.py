25177bf8c898a62867daa5561e69be22c8038b44
Minh-Thien-Bui <97644179+Minh-Thien-Bui@users.noreply.github.com>
2022-02-05 16:37:51 -0800

    Heuristic Update

--- Send More Money.py
+++ Send More Money.py
@@ -1,7 +1,54 @@
 from itertools import permutations
 from time import time
 
-def checkList(testList):
+def heuristicCheck(testList):
+    send = [
+        testList[0],
+        testList[1],
+        testList[2],
+        testList[3]
+        ]
+
+    more = [
+        testList[4],
+        testList[5],
+        testList[6],
+        testList[1]
+        ]
+
+    money = [
+        testList[4],
+        testList[5],
+        testList[2],
+        testList[1],
+        testList[7]
+        ]
+
+    index = len(send) - 1
+    
+    while index >= 0:
+        sumIndex = send[index] + more[index]
+        totalIndex = [
+            money[index + 1],
+            money[index + 1] + 10
+            ]
+
+        if(sumIndex not in totalIndex):
+            return False
+        
+        elif(sumIndex == totalIndex[1]):
+            if(index == 0):
+                for i in testList:
+                    answer.append(i)
+                
+                return True
+
+            else:
+                send[index - 1] += 1
+
+        index -= 1
+
+def bruteForceCheck(testList):
     send = testList[0] * 1000 + testList[1] * 100 + testList[2] * 10 + testList[3]
     more = testList[4] * 1000 + testList[5] * 100 + testList[6] * 10 + testList[1]
     money = testList[4] * 10000 + testList[5] * 1000 + testList[2] * 100 + testList[1] * 10 + testList[7]
@@ -10,6 +57,7 @@ def checkList(testList):
         for index in testList:
             answer.append(index)
 
+# Main Start
 startTime = time()
 numList = []
 answer = []
@@ -21,9 +69,14 @@ permList = list(permutations(numList, 8))
 
 for permutation in permList:
     if permutation[0] != 0 and permutation[4] == 1:
+        if heuristicCheck(permutation):
+            break
+
+'''
         #Check if (D + E = Y) or (D + E = Y + 10) Contributed by Nathaniel Mutkus
         if permutation[3] + permutation[1] == permutation[7] or permutation[3] + permutation[1] == permutation[7] + 10:
-            checkList(permutation)
+            bruteForceCheck(permutation)
+'''
 
 print(len(permList))
 print(answer)
