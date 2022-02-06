9e76602adf1b57285d08c640c3307c548870bb3a
Minh-Thien-Bui <97644179+Minh-Thien-Bui@users.noreply.github.com>
2022-02-06 01:36:33 -0800

    Recursive Update

--- Send More Money.py
+++ Send More Money.py
@@ -1,6 +1,46 @@
 from itertools import permutations
 from time import time
 
+def recursiveCheck(testList, index, sumIndex, totalIndex):
+    if index == len(testList) / 2:
+        if sumIndex == totalIndex:
+            return False
+
+        else:
+            return True
+
+    dnes = [
+        testList[3],
+        testList[2],
+        testList[1],
+        testList[0]
+        ]
+
+    erom = [
+        testList[1],
+        testList[6],
+        testList[5],
+        testList[4]
+        ]
+
+    yenom = [
+        testList[7],
+        testList[1],
+        testList[2],
+        testList[5],
+        testList[4]
+        ]
+
+    sumIndex += dnes[index] * 10 ** index
+    sumIndex += erom[index] * 10 ** index
+    totalIndex += yenom[index] * 10 ** index
+    
+    if sumIndex != totalIndex and sumIndex != totalIndex + 10 ** (index + 1):
+        return False
+
+    else:
+        return recursiveCheck(testList, index + 1, sumIndex, totalIndex)
+
 def heuristicCheck(testList):
     send = [
         testList[0],
@@ -69,10 +109,13 @@ permList = list(permutations(numList, 8))
 
 for permutation in permList:
     if permutation[0] != 0 and permutation[4] == 1:
+        if recursiveCheck(permutation, 0, 0, 0):
+            answer = permutation
+            break
+'''
         if heuristicCheck(permutation):
             break
 
-'''
         #Check if (D + E = Y) or (D + E = Y + 10) Contributed by Nathaniel Mutkus
         if permutation[3] + permutation[1] == permutation[7] or permutation[3] + permutation[1] == permutation[7] + 10:
             bruteForceCheck(permutation)
